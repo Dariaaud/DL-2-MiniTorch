8a86633079a11aa9b2e591d22e39aa598696c3d0
Nekrasova Daria <144384863+Dariaaud@users.noreply.github.com>
2026-09-23 17:54:07 +0300

    Update autodiff.py

--- minitorch/autodiff.py
+++ minitorch/autodiff.py
@@ -64,8 +64,20 @@ def topological_sort(variable: Variable) -> Iterable[Variable]:
     Returns:
         Non-constant Variables in topological order starting from the right.
     """
-    # TODO: Implement for Task 1.4.
-    raise NotImplementedError('Need to implement for Task 1.4')
+    order: List[Variable] = []
+    visited = set()
+
+    def visit(var: Variable) -> None:
+        if var.unique_id in visited or var.is_constant():
+            return
+        if not var.is_leaf():
+            for parent in var.parents:
+                visit(parent)
+        visited.add(var.unique_id)
+        order.insert(0, var)
+
+    visit(variable)
+    return order
 
 
 def backpropagate(variable: Variable, deriv: Any) -> None:
@@ -79,8 +91,19 @@ def backpropagate(variable: Variable, deriv: Any) -> None:
 
     No return. Should write to its results to the derivative values of each leaf through `accumulate_derivative`.
     """
-    # TODO: Implement for Task 1.4.
-    raise NotImplementedError('Need to implement for Task 1.4')
+    order = topological_sort(variable)
+    derivatives = {variable.unique_id: deriv}
+    for var in order:
+        d = derivatives.get(var.unique_id, 0.0)
+        if var.is_leaf():
+            var.accumulate_derivative(d)
+        else:
+            for parent, d_parent in var.chain_rule(d):
+                if parent.is_constant():
+                    continue
+                derivatives[parent.unique_id] = (
+                    derivatives.get(parent.unique_id, 0.0) + d_parent
+                )
 
 
 @dataclass
